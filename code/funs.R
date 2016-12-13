e9d5bc09f12e64591e18ed7daab8a8465a65424d
bjb40 <bryce.bartlett@duke.edu>
2016-12-13 13:58:33 -0500

    general

--- code/funs.R
+++ code/funs.R
@@ -2,3 +2,44 @@
 #general functions for apc project
 #
 #Bryce Bartlett
+
+
+library(ggplot2)
+
+setwindow = function(var,w){
+  #this funciton makes windows from a continuous meaasures
+  #
+  #Input: var = numeric vector
+  #       w = number identifying width of windows
+  
+  return(cut_interval(var,length=w))
+  
+}
+
+
+
+
+
+plt = function(ols,varnames){
+  #this function takes a,p,c measures
+  #and plots them in a panel of 3
+  #dependency -- ggplot2 (can remove for package)
+  #
+  #Input: 
+  #     ols=lm object
+  #     varnames=charater vector (1-3) identifying
+  #     variablenames for age period and cohort (in that order)
+  #     This will work for prefixes...
+  #
+  #Output: list of ggplot objects
+  
+  #select coefficients from 
+  b = coefficients(ols)
+  
+  return(b)
+  
+}
+
+#test
+testb = e.beta[[1]]
+plt(testb,varname=c('a','p','c'))
--- code/sim_luo_hodges.R
+++ code/sim_luo_hodges.R
@@ -10,6 +10,9 @@ rm(list=ls())
 #preliminaries
 library(dplyr); library(ggplot2)
 
+#specific source functions (will become package)
+source('funs.R')
+
 #@@helper funcitons
 
 #generate combinations of 20 ages and 20 periods
@@ -18,11 +21,24 @@ p=a[order(a)]
 c=p-a
 
 combos=data.frame(
-  a=a,a2=a^2,p=p,p2=p^2,c=c,c2=c^2
-); rm(a,p,c)
+  a=a,a2=a^2,
+  p=p,p2=p^2,c=c,c2=c^2
+)
+
+#generate "x" data
+xcov=cov(combos[c('a','p','c')])
+Omega=cov2cor(xcov)
+combos$x.ind=
+  round(runif(nrow(combos),max=max(c),min=min(c)))
+  combos$x.ind2=combos$x.ind^2
+#add a tiny bit of noise that breaks linear dependence
+combos$x.cor = c + runif(nrow(combos),max=1,min=-1)
+combos$x.cor2=combos$x.cor^2
+
+ #rm(a,p,c)
 
 #repeat data for 25 individuals
-dat=combos; for(i in 2:25){dat=rbind(dat,combos)}; rm(combos,i)
+dat=combos; for(i in 2:25){dat=rbind(dat,combos)}; #rm(combos,i)
 #confirm only 400 unique combos
 cat('400 Unique Observations Test:',
   nrow(unique(dat)) == 400, '\n\n'
@@ -43,22 +59,36 @@ print(t.beta)
 
 #draw unique y for each scenario y to y4 in a list of 100 times 
 #/innefficeint memory -- should only store ys
-alldats=list()
+dats=list(); inddats=list(); cordats=list()
 
 for(N in 1:100){
-  alldats[[N]] = dat
+  dats[[N]] = dat[,c('a','a2','p','p2','c','c2')]
+  inddats[[N]] = dat[,c('a','a2','p','p2','x.ind','x.ind2')]
+  cordats[[N]] = dat[,c('a','a2','p','p2','x.cor','x.cor2')]
   for(s in 1:nrow(t.beta)){
     b = as.matrix(t.beta[s,])
-    x=as.matrix(dat[,1:length(b)]);
-   alldats[[N]][,paste0('y',s)]=rnorm(n,mean=x %*% t(b),sd=1)
+    x=as.matrix(dat[,1:length(b)])
+    x.ind=as.matrix(inddats[[N]][,1:length(b)])
+    x.cor=as.matrix(cordats[[N]][,1:length(b)])
+    
+   dats[[N]][,paste0('y',s)]=rnorm(n,mean=x %*% t(b),sd=1)
+   inddats[[N]][,paste0('y',s)]=rnorm(n,mean=x.ind %*% t(b),sd=1)
+   cordats[[N]][,paste0('y',s)]=rnorm(n,mean=x.cor %*% t(b),sd=1)
   }
 }
+
+#####
+#Save and export simulated data
+#####
+
+
+
   
 #plot facets for drawn data of four scenarios (loess smoothing)
 
 #a = reshape(dat,idvar=)
 
-d=ggplot(alldats[[1]],aes(x=a)) + 
+d=ggplot(dats[[1]],aes(x=a)) + 
   geom_smooth(aes(y=y1,col='senario 1')) +
   geom_smooth(aes(y=y2,col='senario 2')) +
   geom_smooth(aes(y=y3,col='senario 3')) +
@@ -70,7 +100,7 @@ print(d)
 t=Sys.time()
 
 #cut window constraints (per luo and hodges)
-x = alldats[[1]] %>%
+x = dats[[1]] %>%
   mutate(
     a=factor(a),
     p=cut_interval(p,length=2),
@@ -95,3 +125,44 @@ cat('Estimated time for one set of constraints:',e.time/60,'minutes \n\n')
 #even bins only (with remainder) --- ignoring double cohort//need function... 
 configs=10^3 #need to really think about this... windows are adjacent.../if I make unequal
 cat('Estimated time for',configs,'window constraints',(e.time*configs)/60/60,'hours\n\n')
+
+
+#some extra crap re ... compoosition by age (Raudenbush-style)
+x$age = as.numeric(levels(x$a)[x$a])
+
+print(
+x %>% group_by(p) %>% summarize(mean(age))
+)
+
+print(
+x %>% group_by(c) %>% summarize(mean(age))
+)
+
+x1 = x %>% group_by(c) %>% mutate(., mcage = age - mean(age),cage=mean(age))
+
+x1 %>% group_by(c) %>% summarize(reg=mean(age),mc=mean(mcage))
+
+print(
+  summary(lm(y1~a+p+c-1,dat=x1))
+)
+
+print(
+  summary(lm(y1~p+c*mcage+mcage^2-1,dat=x1))
+)
+
+####ind v. corr
+
+est_windows = function(){
+  
+}
+
+comp.dats = list(uncorrelated=inddats[[1]],correlated=cordats[[1]])
+for(d in 1:2){
+  colnames(comp.dats[[d]])[5:6]= c('c','c2')
+}
+
+c.beta = lapply(comp.dats,FUN=function(x) 
+  lm(y1~a+a2+p+p2+c+c2,dat=x,x=TRUE))
+
+
+
