f5af2b1562f5987506662c398bdff56e286bfcb8
bjb40 <bryce.bartlett@duke.edu>
2016-12-16 19:51:46 -0500

    weighted mean algorithm

--- code/algorithm.R
+++ code/algorithm.R
@@ -20,7 +20,7 @@ load(paste0(datdir,'testdat.RData'))
 lin_gibbs = function(y,x){
   iter = 1000
   
-  r2=s2=matrix(1,iter)
+  ll=r2=s2=matrix(1,iter)
   b= matrix(0,iter,ncol(x))
   yhat=matrix(0,length(y))
   xtxi = solve(t(x)%*%x)
@@ -33,21 +33,23 @@ lin_gibbs = function(y,x){
   ppd = matrix(0,iter,length(y))
   
   #simulate beta from mvn
-  #you can vetorize this!!
+  #if you can vetorize this you will speed up a lot !!
   for (i in 1:iter){
     b[i,]=pars+t(rnorm(length(pars),mean=0,sd=1))%*%chol(s2[i]*xtxi)
     sse = sum((y-(x %*% b[i,]))^2)
     sst = sum((y-mean(y))^2)
     r2[i] = 1-(sse/sst)
+    ll[i]=sum(dnorm(y,mean=y-(x%*% b[i,]),sd=s2[i],log=TRUE))
   }
   
   colnames(b) = colnames(x)
   ###BIC estimate for Bayes Factor (posterior probability weight)
   ###p. 135, Eq. 26 from Rafferty 1995 (SMR)
   n=length(y)
-  bic=n*log(1-mean(r2))+(nrow(x)-1)*log(n)
+  bic_prime=n*log(1-mean(r2))+(ncol(x)-1)*log(n)
+  bic=2*mean(ll)+ncol(x)*log(n)
   
-  return(list(betas=b,sigma=s2,r2=r2,bic=bic))
+  return(list(betas=b,sigma=s2,r2=r2,bic=bic,bic_prime=bic_prime,ll=ll))
 
 }#end linear gibbs
 
@@ -135,7 +137,106 @@ for(age_w in 0:6){
 } #end age loop
 
 
-#post-processing --- model-averaging based on relative BICs...
+##post-processing --- best model
+bics=unlist(lapply(effects,FUN=function(x) x$bic))
+bics_prime=unlist(lapply(allmods,FUN=function(x) x$bic_prime))
+r2=unlist(lapply(allmods,FUN=function(x) mean(x$r2)))
+
+best=which(bics==min(bics))
+
+print(
+  t(
+    apply(allmods[[best]]$betas,2,FUN=function(x) c(eff=mean(x),sd=sd(x)))
+  )
+)
+
+print(mean(allmods[[best]]$r2))
+
+#cohort plots
+#NOTE that the zero  may be a messed up order in the name!!
+
+library(ggplot2)
+
+load(paste0(datdir,'luo_sim_fits.RData'))
+
+preds=as.data.frame(apply(effects[[best]]$c,2,mean)); colnames(preds)='est'
+rng=apply(effects[[best]]$c,2,quantile,c(0.025,0.975))
+preds$up = rng[2,]
+preds$down = rng[1,]
+preds$actual=pltdat$s1[order(pltdat$cohort)]
+
+g=ggplot(preds,
+         aes(y=est,x=1:nrow(preds))) + 
+  geom_point() + 
+  geom_errorbar(aes(ymax=up,ymin=down)) +
+  geom_line(aes(y=actual))
+
+print(g)
+
+##post-processing -- model averaging
+#averaging algorithm from ... eq 35 from Rafferty SMR
 
 
- 
+
+
+#w = exp(-.5*bics)/sum(exp(-.5*bics))
+#w_prime=exp(-.5*bics_prime)/sum(exp(-.5*bics_prime))
+
+#there are severe underflow problems.... since this is just a proportionality issue
+#/changing proportion
+prop=0.000005
+w = exp(prop*bics)/sum(exp(prop*bics))
+
+for(m in seq_along(effects)){
+  effects[[m]]$w=w[m]
+}
+
+#weighted mean
+
+#for(l in seq_along(effects)){
+#  for(t in c('a','p','c')){
+#    if(is.null(effects[[l]][[t]])){
+#      effects[[l]][[t]] = 0
+#    }
+#  }
+#}
+
+wtmn=function(l,var,w){
+  #makes weighted mean
+  #l is list results
+  #var is charactername
+  r=NA
+  if(is.null(l[[var]])){r=0} else{
+    r=apply(l[[var]],2,mean)*w
+  }
+  return(r)
+}
+
+preds$m_est =  rowSums(
+  as.data.frame(
+      lapply(effects,FUN=function(e)
+            wtmn(e,'c',e$w))
+))
+
+rng2=lapply(
+  effects,FUN=function(e)
+    apply(e,2,quantile,c(0.025,0.975))
+  )
+
+
+  #as.data.frame(apply(effects[[best]]$c,2,mean)); colnames(preds)='est'
+
+
+
+library(ggplot2); library(reshape2)
+preds$cohort=1:nrow(preds)
+#predsm=preds[,! colnames(preds) %in% c('up','down')]
+#pp=melt(predsm,id='cohort')
+
+m=ggplot(preds,
+         aes(y=m_est,x=cohort)) + 
+         geom_point() + geom_line(aes(y=actual))
+
+       
+
+print(m)  
