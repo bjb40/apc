1b3d405368d30b2fca0ebe6448ad278a1165fff6
bjb40 <bryce.bartlett@duke.edu>
2018-03-26 18:09:16 -0400

    almost there...

--- code/sim_r2.R
+++ code/sim_r2.R
@@ -5,47 +5,126 @@
 #rm(list=ls())
 source('config~.R')
 
-#tbeta defined by simulator.R
-load(file=paste0(datdir,'sim2_tbeta.RData'))
+###############
+#DRAW DATA
+##############
+
+#####
+#SET DIMENSIONS FOR A and P (c wil be derived)
 
 #preliminaries
 library(dplyr)
 
-a=rep(1:20,20)
-p=a[order(a)]
-c=p-a
-
-combos=data.frame(
-  a=a,a2=a^2,
-  p=p,p2=p^2,c=c,c2=c^2
-)
-
-dat=combos; for(i in 2:25){dat=rbind(dat,combos)}; #rm(combos,i)
-#confirm only 400 unique combos
-cat('400 Unique Observations Test:',
-    nrow(unique(dat)) == 400, '\n\n'
-)
+###
+#draw random set of ages over a 
+#certain number of periods (i.e.) 
+#survey years (30) based on GSS 
+p=seq(1972,2014,by=2)
+#loop over p to create a draw of a,p,and c
+tdat = lapply(p, function(y) {
+  a=round(rnorm(2500,mean=46,sd=17.5)) # gss mean
+  a = a[a>17 & a<79]
+  df = data.frame(a=a,p=rep(y,length(a)))
+  df$c=df$p-df$a
+  return(df)
+})
+#bind
+tdat = do.call(rbind,tdat)
 
 n=nrow(dat)
+
+####
+#draw random effects values for various functions
+tdat$a2 = (tdat$a-mean(tdat$a))^2
+tdat$ac = tdat$a-mean(tdat$a)
+#age effects = quadratic / centered on mean age
+age = runif(1,-1,1)/10; age2=runif(1,-1,1)/10
+
+a.eff = as.matrix(tdat[,c('ac','a2')]) %*% 
+  matrix(c(age,age2),2,1)
+range(a.eff)
+plot(tdat$a,a.eff)
+
+draw_blocks = function(breakprob,autocorr,var){
+    #breakprob is probability of a window break between 0 and 1
+    #autocorr is an SD of effect sizes for a random walk
+    #var is the variable to draw across rndomly
+    
+    #returns list of (1) betas and (2) calculated effects for data in var
+  
+  #period and cohort effects have probability of "jump" 
+  #and "autocorrelation"
+  #breakprob = 0.3
+  #autocorr = diff(range(a.eff))/5
+  all.var = unique(var)
+  #pull random breaks
+  breaks.var = c(min(var)-1,
+               all.var[as.logical(
+                 rbinom(length(all.var),1,breakprob)
+                 )])
+  #make sure it covers the whole place
+  if(max(breaks.var)<max(var)){
+    breaks.var = c(breaks.var,max(var))
+  }
+  #draw random disturbance
+  var.beta = rnorm(length(breaks.var)-1,sd=autocorr)
+  #random walk from beginning
+  var.beta = cumsum(var.beta)
+  
+  ##calculate effects
+  #window is a method for apcwin that creates a factor
+  vv=data.frame(v=window(var,breaks=breaks.var))
+  var.eff = model.matrix(~v-1,data=vv) %*% 
+    matrix(var.beta,length(var.beta),1)
+  
+  #rename
+  names(var.beta) = levels(vv$v)
+  
+  return(list(
+    var.beta=var.beta,var.eff=var.eff))
+
+}
+
+pdraw = draw_blocks(breakprob=0.3,
+                    autocorr=diff(range(a.eff))/5, #1/5 of age effect
+                    var=tdat$p)
+
+period=pdraw$var.beta
+plot(tdat$p,pdraw$var.eff)
+
+cdraw = draw_blocks(breakprob=0.3,
+                    autocorr=diff(range(a.eff))/5,
+                    var=tdat$c)
+
+cohort=cdraw$var.beta
+plot(tdat$c,cdraw$var.eff)
+
+tdat$yhat = a.eff + pdraw$var.eff + cdraw$var.eff
+
 
 r2=0.03
 
 evar=(1-r2)/r2
-e=rnorm(n,0,sqrt(evar))
+e=rnorm(nrow(tdat),0,sqrt(evar))
 
-#simulate and save data
-dat$s1 = as.vector(as.matrix(dat)%*%t(t.beta))
-dat$y1 = dat$s1 + e
+tdat$y = tdat$yhat + e
 #tdat = dat
-tdat = dat %>% dplyr::select(-c,-a2,-p2,-c2)
+#tdat = dat %>% dplyr::select(-c,-a2,-p2,-c2)
 
 print(
-  summary(lm(y1~a+p,data=tdat))
+  summary(lm(y~a+p,data=tdat))
 )
 
-save(tdat,file=paste0(datdir,'nsim.RData'))
+est = apcsamp(dat=tdat,dv='y',
+              cores=4,method='ml',
+              chains=4,samples=250)
 
-pltdat=list()
+summary(est)
+
+effs = draw_effs(est)
+
+#save(tdat,file=paste0(datdir,'nsim.RData'))
+#pltdat=list()
 
 #pull unique data -- should make this a loop
 uns = list(
--- code/simulator.R
+++ code/simulator.R
@@ -13,18 +13,8 @@ for(drawsimulations in 1:5){
   cat('Simulation',drawsimulations,'...\n')
   #funciton is nested because sources below delete all in memory
   source('config~.R')
-  draw_betas = function(){
-    #draws random set of betas
-    t.beta=data.frame(t(runif(6,-1,1)))
-    colnames(t.beta)=c('a','a2','p','p2','c','c2')
-    #rescale quadratic effects to be smaller
-    t.beta[,c('a2','p2','c2')] = t.beta[,c('a2','p2','c2')]/10
-    #print(t.beta)
-    save(t.beta,file=paste0(datdir,'sim2_tbeta.RData'))
-  }
-  
-  draw_betas()   
   source('sim_r2.R') 
+  draw_betas()#funciton in sim_r2.R   
   #source('algorithm.R')
   library(apcwin)
   #apcsamp()
